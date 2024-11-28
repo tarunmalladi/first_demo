3c76ecb52d9fa43d377e4e0dfea6fd7f865f6a4e
tarunmalladi <malladitarun100@gmail.com>
2024-11-28 11:23:00 +0530

    p2 is added to master branch

---
+++ p2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+printf("enter the number:\n");
+scanf("%d",&num);
+if(num%2==0)
+	printf("%d is even_num",num);
+else
+	printf("%d is odd_num",num);
+}
